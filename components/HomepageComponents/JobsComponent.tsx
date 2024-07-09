beea2c8b3e37eb6827db98dbe0297c87f7dd21ab
Perpy-del <perpetualmeninwa@gmail.com>
2024-07-09 14:40:02 +0100

    feat: :sparkles: add animations for the jobs component and category

--- components/HomepageComponents/JobsComponent.tsx
+++ components/HomepageComponents/JobsComponent.tsx
@@ -15,25 +15,25 @@ const JobsComponent = (props: Props) => {
       {/* Latest Job Displayed */}
       <div className='grid grid-cols-3 gap-5 pb-[60px]'>
         {jobData.map((job: JobDataInterface, index: number) => (
-          <div key={index} className='p-5 rounded-[20px] border border-recSubtle dark:border-[#242424] border-l-[8px] cursor-pointer' style={{borderLeftColor: job.sideColor}}>
+          <div key={index} className='p-5 rounded-[20px] border border-recSubtle dark:border-[#242424] border-l-[8px] cursor-pointer hover:scale-105 transition transform duration-500' style={{borderLeftColor: job.sideColor}}>
             <div className='flex items-center justify-between text-textSmall font-recMedium pb-8'>
               <span>Type: {job.type}</span>
               <span className='text-recSubtle'>{job.timePosted}</span>
             </div>
-            <h3 className='font-recBold text-headerSix'>{job.job}</h3>
+            <h3 className='font-recBold text-headerSix pb-2 hover:underline'>{job.job}</h3>
             <p className='btn-text text-recSubtle dark:text-recLightGrey pb-8'>{job.salaryRange}</p> 
             <div className='flex justify-between items-center'>
               <div className='btn-text text-recSubtle dark:text-recLightGrey pb-2 flex items-center gap-1'>
                 <MapPin className="text-recSubtle dark:text-recLightGrey" />
                 <span>{job.jobLocation}</span>
               </div>
-              <div className='btn-text text-recSubtle dark:text-recLightGrey pb-2 flex items-center gap-1'>
+              {/* <div className='btn-text text-recSubtle dark:text-recLightGrey pb-2 flex items-center gap-1'>
                 <IoMdRadioButtonOn
                   className="text-recSubtle dark:text-recLightGrey"
                   size={24}
                 />
                 <span>{job.location}</span>
-              </div>
+              </div> */}
             </div>
           </div>
         ))}
