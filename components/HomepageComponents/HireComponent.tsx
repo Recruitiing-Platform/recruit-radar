9338f32ed576fa33c77ebfcf95d753e4e4e92dd8
Perpy-del <perpetualmeninwa@gmail.com>
2024-07-10 13:34:28 +0100

    feat: :sparkles: add responsiveness to the choose section

--- components/HomepageComponents/HireComponent.tsx
+++ components/HomepageComponents/HireComponent.tsx
@@ -8,7 +8,7 @@ type Props = {};
 
 const HireComponent = (props: Props) => {
   return (
-    <div className="px-10 pb-40">
+    <div className="sm:hidden md:block md:pr-12 md:pl-3 lg:pl-32 lg:pr-44 xl:px-10 pb-40">
       <div className='relative h-[450px]'>
         {rotateLinkData.map((link: LinkDataInterface, index: number) => (
           <div key={index} className='object-center object-cover' style={{position: "absolute", top: link.top, left: link.left}}>
@@ -21,10 +21,10 @@ const HireComponent = (props: Props) => {
             />
           </div>
         ))}
-        <div className='absolute top-[40%] left-[22%] h-64 w-[700px] bg-recPrimary/25 rounded-full blur-effect'></div>
-        <div className='absolute top-[40%] left-[25%] w-1/2 text-center'>
+        <div className='absolute top-[30%] lg:top-[40%] left-[22%] h-80 xl:h-64 w-[480px] xl:w-[700px] bg-recPrimary/25 rounded-full blur-effect'></div>
+        <div className='absolute top-[30%] lg:top-[40%] right-[20%] lg:left-[25%] md:w-1/2 lg:w-[60%] xl:w-1/2 text-center'>
             <h2 className='display-two pb-6'>Ready for your next hire</h2>
-            <p className='text-headerSix font-recRegular leading-8 pb-11'>Along with conventional advertising & below the line activities & organiztions have come to realize that they need to invest</p>
+            <p className='text-headerSix font-recRegular leading-8 md:pb-7 lg:pb-11'>Along with conventional advertising & below the line activities & organiztions have come to realize that they need to invest</p>
             <Button className='block mx-auto w-60 text-headerSix font-recMedium rounded-3xl hover:scale-105 ease-in-out transition transform duration-300' style={{color: "#F0F0F0"}}>Create account</Button>
         </div>
       </div>
