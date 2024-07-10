25b608ead890560c9305c1dbf93a61213cbc4122
Perpy-del <perpetualmeninwa@gmail.com>
2024-07-10 16:29:52 +0100

    feat: :sparkles: add responsiveness to the testimonial section

--- components/CarouselNavComponent.tsx
+++ components/CarouselNavComponent.tsx
@@ -8,13 +8,13 @@ import {
   Carousel,
   CarouselContent,
   CarouselItem,
-  CarouselNext,
-  CarouselPrevious,
+  CarouselNavNext,
+  CarouselNavPrevious,
 } from '@/components/ui/carousel';
 import { testimonialData } from '@/data';
 import { TestimonialDataInterface } from '@/interfaces/HomepageInterface';
 
-export function CarouselPlugin() {
+export function CarouselNavPlugin() {
     // const [activeIndex, setActiveIndex] = React.useState(0);
   const plugin = React.useRef(
     Autoplay({ delay: 4000, stopOnInteraction: true })
@@ -27,16 +27,16 @@ export function CarouselPlugin() {
   return (
     <Carousel
       plugins={[plugin.current]}
-      className="lg:max-w-[620px] xl:max-w-[900px] xxl:max-w-[1440px]"
+      className="flex flex-col"
     >
-      <CarouselContent className=''>
+      <CarouselContent className='sm:max-w-[280px] sml:max-w-[320px] smd:max-w-[425px] mdm:max-w-[370px] mdl:max-w-[400px] md:max-w-[468px] mdg:max-w-[600px]'>
         {testimonialData.map((data: TestimonialDataInterface, index: number) => (
           <CarouselItem key={index}>
             <div className="p-1">
               <Card className='border-0 bg-recLightGrey dark:bg-[#000] rounded-2xl'>
                 <CardContent className="p-5">
-                  <h3 className='text-headerSix font-recSemiBold pb-2'>{data.name}, {data.job}</h3>
-                  <p>{data.testimony}</p>
+                  <h3 className='text-[18px] font-recSemiBold pb-2 md:pb-5 lg:pb-2 w-2/3'>{data.name}, {data.job}</h3>
+                  <p className='text-textSmall md:text-textBody'>{data.testimony}</p>
                 </CardContent>
               </Card>
             </div>
@@ -44,8 +44,8 @@ export function CarouselPlugin() {
         ))}
       </CarouselContent>
         <div className="flex items-center gap-3">
-          <CarouselPrevious />
-          <CarouselNext />
+          <CarouselNavPrevious />
+          <CarouselNavNext />
         </div>
     </Carousel>
   );
--- components/ui/carousel.tsx
+++ components/ui/carousel.tsx
@@ -252,6 +252,64 @@ const CarouselNext = React.forwardRef<
 })
 CarouselNext.displayName = "CarouselNext"
 
+const CarouselNavPrevious = React.forwardRef<
+  HTMLButtonElement,
+  React.ComponentProps<typeof Button>
+>(({ className, variant = "outline", size = "icon", ...props }, ref) => {
+  const { orientation, scrollPrev, canScrollPrev } = useCarousel()
+
+  return (
+    <Button
+      ref={ref}
+      variant={variant}
+      size={size}
+      className={cn(
+        "absolute h-8 w-8 rounded-full",
+        orientation === "horizontal"
+          ? "right-[75px] top-[15%] -translate-y-1/2"
+          : "-top-12 left-1/2 -translate-x-1/2 rotate-90",
+        className
+      )}
+      disabled={!canScrollPrev}
+      onClick={scrollPrev}
+      {...props}
+    >
+      <ArrowLeft className="h-4 w-4" />
+      <span className="sr-only">Previous slide</span>
+    </Button>
+  )
+})
+CarouselNavPrevious.displayName = "CarouselNavPrevious"
+
+const CarouselNavNext = React.forwardRef<
+  HTMLButtonElement,
+  React.ComponentProps<typeof Button>
+>(({ className, variant = "outline", size = "icon", ...props }, ref) => {
+  const { orientation, scrollNext, canScrollNext } = useCarousel()
+
+  return (
+    <Button
+      ref={ref}
+      variant={variant}
+      size={size}
+      className={cn(
+        "absolute h-8 w-8 rounded-full",
+        orientation === "horizontal"
+          ? "right-8 top-[15%] -translate-y-1/2"
+          : "-bottom-12 left-1/2 -translate-x-1/2 rotate-90",
+        className
+      )}
+      disabled={!canScrollNext}
+      onClick={scrollNext}
+      {...props}
+    >
+      <ArrowRight className="h-4 w-4" />
+      <span className="sr-only">Next slide</span>
+    </Button>
+  )
+})
+CarouselNavNext.displayName = "CarouselNavNext"
+
 export {
   type CarouselApi,
   Carousel,
@@ -259,4 +317,6 @@ export {
   CarouselItem,
   CarouselPrevious,
   CarouselNext,
+  CarouselNavPrevious,
+  CarouselNavNext,
 }
