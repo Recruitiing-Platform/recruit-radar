3e00dc6b7a102b00920837fbcf674c51e4b386a4
Perpy-del <perpetualmeninwa@gmail.com>
2024-08-02 14:29:45 +0100

    fix: :lipstick: add responsiveness to all signup pages

--- app/(auth)/verify/page.tsx
+++ app/(auth)/verify/page.tsx
@@ -10,26 +10,26 @@ import { ModeToggle } from '@/components/ModeToggle';
 const VerifyPage = () => {
   const router = useRouter();
 
-  useEffect(() => {
-    const verifyEmail = async () => {
-      const oobCode = new URLSearchParams(window.location.search).get(
-        'oobCode'
-      ) as string;
+  // useEffect(() => {
+  //   const verifyEmail = async () => {
+  //     const oobCode = new URLSearchParams(window.location.search).get(
+  //       'oobCode'
+  //     ) as string;
 
-      if (!oobCode) {
-        router.push('/error');
-      }
+  //     if (!oobCode) {
+  //       router.push('/error');
+  //     }
 
-      try {
-        await applyActionCode(auth, oobCode);
-        router.push('/success');
-      } catch (error) {
-        console.error('Error verifying email:', error);
-      }
-    };
+  //     try {
+  //       await applyActionCode(auth, oobCode);
+  //       router.push('/success');
+  //     } catch (error) {
+  //       console.error('Error verifying email:', error);
+  //     }
+  //   };
 
-    verifyEmail();
-  }, [router]);
+  //   verifyEmail();
+  // }, [router]);
 
   return (
     <div className="px-5 md:px-10 lg:px-20 pt-5">
@@ -38,10 +38,10 @@ const VerifyPage = () => {
       </div>
       <div className="text-recDark dark:text-recLightGrey flex flex-col items-center justify-center h-[80vh]">
         <FaEnvelopeOpenText size={92} />
-        <p className="text-headerTwo font-recBold pt-6 pb-2">
+        <p className="text-headerFour text-center md:text-headerTwo font-recBold pt-6 pb-2">
           Verifying your email...
         </p>
-        <p className="text-[18px]">
+        <p className="text-textSmall md:text-[18px] text-center">
           Almost there... we are currently verifying your email...
         </p>
       </div>
