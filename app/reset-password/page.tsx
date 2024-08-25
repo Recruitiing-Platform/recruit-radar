ebec41862c506cc3f00bbd547bebd75838c29532
mutintamundia00 <148071393+mutintamundia00@users.noreply.github.com>
2024-08-25 17:52:23 +0200

    decisionpage worked on

---
+++ app/reset-password/page.tsx
@@ -0,0 +1,10 @@
+<div className='bg-recSuccess w-[766px] h-[1024px]'>
+<div className='w-[462px] h-[312px] mt-[288px] ml-[172px] bg-[#FFFFFF] flex justify-between'>
+<h2 className=' w-[462px] h-[40px] text-[32px] pl-4 font-recBold  text-[#000000] font-sans align-middle'>Please select your user type</h2>
+    <div className='w-[179px] h-[191px] bg-recError mt-[61px]'>
+        <div className='w-[92px] h-[92px] bg-[#AAB0BB] rounded-[10000px] ml-[36px] pt-[11px] pb-[5px] pl-[11px] pr-[5px] '>
+         <img src="/vec.jpg" alt="employer vector" />
+       </div>
+    </div>
+</div>         
+</div>
