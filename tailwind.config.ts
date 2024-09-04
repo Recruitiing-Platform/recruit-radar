81ddd859faf7de909b6b44fc4a19b3caf07cf626
Perpy-del <perpetualmeninwa@gmail.com>
2024-09-04 13:17:19 +0100

    feat: :sparkles: add the main profile page

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -114,10 +114,20 @@ const config = {
           from: { height: "var(--radix-accordion-content-height)" },
           to: { height: "0" },
         },
+        marquee: {
+          from: { transform: "translateX(0)" },
+          to: { transform: "translateX(calc(-100% - var(--gap)))" },
+        },
+        "marquee-vertical": {
+          from: { transform: "translateY(0)" },
+          to: { transform: "translateY(calc(-100% - var(--gap)))" },
+        },
       },
       animation: {
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
+        marquee: "marquee var(--duration) linear infinite",
+        "marquee-vertical": "marquee-vertical var(--duration) linear infinite",
       },
       boxShadow: {
         'employer': "62px 137px 42px 0px rgba(255, 255, 255, 0.00), 40px 88px 39px 0px rgba(255, 255, 255, 0.01), 22px 49px 33px 0px rgba(255, 255, 255, 0.05), 10px 22px 24px 0px rgba(255, 255, 255, 0.09), 2px 5px 13px 0px rgba(255, 255, 255, 0.10);",
