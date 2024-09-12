90d47cf484be6f7b8db55a506565dca763437c80
ekestephanie1 <ekestephanie123@gmail.com>
2024-09-12 21:00:34 +0100

    applied job page completed on large screen

--- interfaces/ProfileInterface.ts
+++ interfaces/ProfileInterface.ts
@@ -1,7 +1,19 @@
 export interface ProfileDataInterface {
-  header: string;
-  subheader: string;
+    header: string;
+    subheader: string;
 }
+
+export interface AppliedJobDataInterface {
+  jobTitle: string;
+  employerName: string;
+  applicationDate: string;
+  salary: string;
+  status: string;
+  datePosted: string;
+  location: string;
+}
+  
+
 
 export interface JobAlerts {
   logo: string;
