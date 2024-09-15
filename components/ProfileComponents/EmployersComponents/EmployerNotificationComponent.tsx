c750427f4db6b2813d4e33f345c02a48acc99af1
Perpy-del <perpetualmeninwa@gmail.com>
2024-09-15 23:38:44 +0100

    refactor: :iphone: little fixes on responsiveness

--- components/ProfileComponents/EmployersComponents/EmployerNotificationComponent.tsx
+++ components/ProfileComponents/EmployersComponents/EmployerNotificationComponent.tsx
@@ -1,20 +1,14 @@
 import React from "react";
 import { IoMdNotificationsOutline } from "react-icons/io";
 import { Switch } from "@/components/ui/switch";
-import { EmployerNotification } from "@/interfaces/ProfileInterface";
 import { profiles } from "@/data";
-import Image from "next/image";
 import ProfileCardComponent from "./ProfileCardComponent";
-type Props = {};
 
-
-
-
-const EmployerNotificationComponent = (props: Props) => {
+const EmployerNotificationComponent = () => {
   return (
     <div className="p-5 md:p-10 lg:pr-20">
       <h1 className="text-headerFive font-recSemiBold pb-3">Notification</h1>
-      <div className="flex items-center justify-between p-4 border border-[#808080]/50 rounded-lg mb-14 gap-3 md:gap-0">
+      <div className="flex items-center justify-between p-4 border border-[#808080]/50 rounded-lg mb-8 gap-3 md:gap-0">
         <div className="flex items-center gap-2 py-3">
           <span className="hidden md:flex p-2.5 bg-recSecondary text-recAccent rounded-lg">
             <IoMdNotificationsOutline size={32} />
@@ -30,14 +24,14 @@ const EmployerNotificationComponent = (props: Props) => {
         </div>
         <Switch />
       </div>
-      <div className="bg-recLightGrey dark:bg-[#000611]  h-fit py-8 rounded-md">
+      <div className="bg-recLightGrey dark:bg-[#000611] h-fit pb-8 rounded-md">
         <div className="flex justify-between sm:py-4">
           <div className="px-4 ">All Application (64)</div>
           <a href="" className="px-4">
             View all
           </a>
         </div>
-        <div className=" lg:my-8 lg:px-4  grid lg:grid-cols-3 grid-col-1 gap-4">
+        <div className=" lg:my-8 grid md:grid-cols-2 xl:grid-cols-3 grid-col-1 gap-4">
           {profiles.map((t, card) => (
             <div className="" key={card}>
               <ProfileCardComponent profile={t} />
@@ -53,7 +47,7 @@ const EmployerNotificationComponent = (props: Props) => {
               View all
             </a>
           </div>
-          <div className=" my-8 px-4  grid lg:grid-cols-3 grid-col-1 gap-4">
+          <div className="pt-4 lg:my-8 grid md:grid-cols-2 xl:grid-cols-3 grid-col-1 gap-4">
             {profiles.map((t, card) => (
               <div className="" key={card}>
                 <ProfileCardComponent profile={t} />
